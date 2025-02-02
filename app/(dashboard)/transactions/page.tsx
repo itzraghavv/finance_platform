ea45b43e517ef45c0060736346adb7f05395f1ce
raghav-97 <raghavtandulkar97@gmail.com>
2025-02-02 15:56:15 +0530

    working on transaction form

---
+++ app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
+
+import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts";
+
+export default function TransactionsPage() {
+  const newTransaction = useNewTransaction();
+  const accountQuery = useGetAccounts();
+  const deleteAccounts = useBulkDeleteAccounts();
+
+  const accounts = accountQuery.data || [];
+  const isDisabled = accountQuery.isLoading || deleteAccounts.isPending;
+
+  if (accountQuery.isLoading) {
+    return (
+      <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-400 animate-spin" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Transaction History</CardTitle>
+          <Button onClick={newTransaction.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add New
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            filterKey="name"
+            columns={columns}
+            data={accounts}
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteAccounts.mutate({ ids });
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -1,10 +1,11 @@
 import { z } from "zod";
+import { insertAccountSchema, insertTransactionsSchema } from "@/db/schema";
+
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Trash } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { insertAccountSchema } from "@/db/schema";
 import {
   Form,
   FormMessage,
@@ -14,21 +15,35 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 
-const formSchema = insertAccountSchema.pick({
-  name: true,
+const formSchema = z.object({
+  date: z.coerce.date(),
+  accountId: z.string(),
+  categoryId: z.string().nullable().optional(),
+  payee: z.string(),
+  amount: z.string(),
+  notes: z.string().nullable().optional(),
 });
 
+const apiSchema = insertTransactionsSchema.omit({
+  id: true;
+})
+
 type FormValues = z.input<typeof formSchema>;
+type ApiFormValues = z.input<typeof apiSchema>;
 
 type Props = {
   id?: string;
   defaultValues?: FormValues;
-  onSubmit: (values: FormValues) => void;
+  onSubmit: (values: ApiFormValues) => void;
   onDelete?: () => void;
   disabled?: boolean;
+  accountOption: { label: string, value: string; } [];
+  categoryOption: {label: string, value: string;} [];
+  onCreateCategory: (name: string) => void;
+  onCreateAccount: (name: string) => void;
 };
 
-export const AccountForm = ({
+export const TransactionForm = ({
   id,
   defaultValues,
   onSubmit,
@@ -41,7 +56,7 @@ export const AccountForm = ({
   });
 
   const handleSubmit = (values: FormValues) => {
-    onSubmit(values)
+    onSubmit(values);
   };
 
   const handleDelete = () => {
@@ -75,16 +90,18 @@ export const AccountForm = ({
         <Button className="w-full" disabled={disabled}>
           {id ? "Save changes" : "Create Account"}
         </Button>
-       {!!id && <Button
-          onClick={handleDelete}
-          className="w-full"
-          variant="outline"
-          type="button"
-          disabled={disabled}
-        >
-          <Trash className="size-4 mr-2"/>
-          Delete Account
-        </Button>}
+        {!!id && (
+          <Button
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+            type="button"
+            disabled={disabled}
+          >
+            <Trash className="size-4 mr-2" />
+            Delete Account
+          </Button>
+        )}
       </form>
     </Form>
   );
